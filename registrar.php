9da04479529b1a722f9963cbedbd2ed359e443cc
GanGss2 <gangss2@hotmail.com>
2013-06-11 13:40:01 -0300

    AJUSTES

--- registrar.php
+++ registrar.php
@@ -11,7 +11,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
         <!--FB-->
-        <script src="src/API/fb.api.js" type="text/javascript"></script>
+        <script src="src/API/fb/fb.api.js" type="text/javascript"></script>
         <!--FB-->
         <!--Basico-->
         <script src="src/js/basic/AjaxRequest.class.js" type="text/javascript"></script>
