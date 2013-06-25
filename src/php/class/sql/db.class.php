d1c42bf40f4bebfd5cd5a307a74b988f74af1957
GanGss2 <gangss2@hotmail.com>
2013-06-25 16:32:49 -0300

    Cont -> process.class.sc.php | Fix -> db.class.php

--- src/php/class/sql/db.class.php
+++ src/php/class/sql/db.class.php
@@ -5,14 +5,16 @@ class db {
     private $host;
     private $login_db;
     private $senha_db;
+    private $database;
     //
     private $db; // ligação
     private $basedados; // database
 
     function __construct($iniCfgs) {
-        $this->host = $cfgs->getHost();
-        $this->senha_db = $cfgs->getSenha_db();
-        $this->login_db = $cfgs->getLogin_db();
+        $this->host = $iniCfgs->getHost();
+        $this->senha_db = $iniCfgs->getSenha_db();
+        $this->login_db = $iniCfgs->getLogin_db();
+        $this->database = $iniCfgs->getDatabase();
 
         $this->db = mysql_connect($this->host, $this->login_db, $this->senha_db);
         $this->basedados = mysql_select_db($this->database, $this->db);
